f9ae1f49a779833bdc505058d33112f14fdf35c8
jeroengerits <jeroen@gerits.email>
2021-01-23 20:01:15 +0100

    ⚡️ add babel api cache

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+  };
 };
